8e6b185d869f13c54ed93f2b562a4f8e3cd61ba7
kris238 <sergejsakirov464@gmail.com>
2025-05-23 22:37:59 +0700

    bot

--- 4pr/4.py
+++ 4pr/4.py
@@ -3,5 +3,5 @@ def dt(date):
     d = datetime.strptime(date, "%d.%m.%Y")
     new_date = d.strftime("%A, %d %B, %Y год")
     return new_date
-date = input("Введите дату(dd-mm-yyyy):")
+date = input("Введите дату(dd.mm.yyyy):")
 print(dt(date))
