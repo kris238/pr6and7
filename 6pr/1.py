a5007acb35f570d5a5cf182f4532e569c65d1dc3
kris238 <sergejsakirov464@gmail.com>
2025-06-16 21:21:37 +0700

    6.1z

--- 6pr/1.py
+++ 6pr/1.py
@@ -2,4 +2,3 @@ names = ["Анна", "Алексей", "Ольга", "Артем", "Мария",
          "Денис", "Анастасия", "Сергей", "Ирина", "Андрей"]
 names_a = [name for name in names if name.startswith('А')]
 print(names_a)
-
